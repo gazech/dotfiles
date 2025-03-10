c42671ff6f936cdcbbe58cbb7b6cecd1f2d97e7a
gazech <gazecki.hunt@gmail.com>
2025-03-09 17:28:02 -0700

    added init script for centralized env var

--- purge_backups.sh
+++ purge_backups.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/bash
 
-if [[ -z ${DOTFILE_BACKUP_EXTENSION} ]]; then
-    echo -e "\033[0;31mDOTFILE_BACKUP_EXTENSION env variable not found; exiting...\033[0m"
-    return 1
-fi
-
-if [[ -z ${DOTFILE_DST_DIR} ]]; then
-    echo "\033[0;31mDOTFILE_DST_DIR env variable not found; exiting...\033[0m"
-    return 1
+if [[ -z ${DOTFILE_BACKUP_EXTENSION} ]] || [[ -z ${DOTFILE_DST_DIR} ]] || [[ -z ${_INDENT} ]]; then
+    echo -e "\033[0;31mdotfile env variables not found, run \`dfinit.sh\` first; exiting...\033[0m"
+    # Return for sourced scripts, exit for direct execution
+    [[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 1 || exit 1
 fi
 
 REPO_PATH=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 DOTFILE_PATH=${REPO_PATH}/${DOTFILE_DIRNAME}
 
+ctr=0 
 for dot in `find ${DOTFILE_PATH} -type f`; do
 
     DOT_NAME=`basename ${dot}`
@@ -20,9 +17,12 @@ for dot in `find ${DOTFILE_PATH} -type f`; do
 
     echo -e "\033[1munlinking the following for ${DOT_NAME}:\033[0m"
     for lnk in `find ${DOTFILE_DST_DIR} -name "${DOT_NAME}*${DOTFILE_BACKUP_EXTENSION}*" -type l`; do
-        echo -e "    \033[0;90m${lnk}\033[0m"
+        echo -e "    \033[2m${lnk}\033[0m"
         unlink ${lnk}
+        ((ctr++))
     done
     echo ""
 
 done
+
+echo -e "\033[42m >> Donezo (${ctr} dotfile backups deleted) << \033[0m"
